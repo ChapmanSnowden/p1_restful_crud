910ac8becb02a642f6e34cd3e3dc1b1a1062a69b
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-13 10:46:31 -0700

    able to add note

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,8 +1,12 @@
 get '/' do
   # Look in app/views/index.erb
+  @notes = Note.all
   erb :index
 end
 
 post '/' do
+  new = Note.new(title: params[:title], content: params[:content])
+  new.save
+  redirect '/'
 end
 
